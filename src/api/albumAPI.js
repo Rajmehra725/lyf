ccc30ad7bb9fa8019101bff34a3ec5f48bca9ff5
Rajmehra725 <nagiyaraj.725@gmail.com>
2025-11-20 21:03:35 +0530

    Updated albumAPI and QRCodeModal components

--- src/api/albumAPI.js
+++ src/api/albumAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/albums",
+  baseURL: "https://raaznotes-backend.onrender.com/api/albums",
 });
 
 // 👉 Create album
